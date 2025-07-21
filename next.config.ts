ae555987620293a8cf9edf2849ab455d5dc709be
Tucker Ritti <45155694+tuckerritti@users.noreply.github.com>
2025-07-20 21:50:35 -0700

    Sub path 2

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/taka-frontend/",
+  basePath: "/taka-frontend",
   trailingSlash: true,
   images: {
     unoptimized: true,
